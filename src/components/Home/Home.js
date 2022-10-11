71b528b856d7ca67a605a80db8619f9ac8e71e74
01792166684 <tanzinatania491991@gmail.com>
2022-10-11 21:24:00 +0600

    header added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-          <h1>thus is home page</h1>  
+        <div className="bg-local  "  style= {{backgroundImage:`url("https://tse1.mm.bing.net/th?id=OIP.yxPZjg64ooWeC6SST-ckFAHaEo&pid=Api&P=0")`,backgroundSize:"cover",objectFit:"cover"}}>
+          <h1 className='text-center font-bold py-10'>Hey , Let's play quiz .</h1> 
+          <p className='text-center font-bold pb-10 '>It's the Quiz time</p> 
         </div>
     );
 };
