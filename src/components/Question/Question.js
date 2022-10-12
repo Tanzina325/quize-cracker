fcc8ef8dd3f4641f69ca247cf93a7fcef4fe064d
01792166684 <tanzinatania491991@gmail.com>
2022-10-12 09:53:59 +0600

    question page styled

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -15,9 +15,9 @@ const Question = ({questionMark}) => {
         <div className='border-solid border-4 rounded-3xl border-sky-500 p-5 m-5'>
         <div className="flex justify-around">
             <h1 className="font-bold">{question.slice(3,-4)}</h1>
-            <p><FontAwesomeIcon icon={faEye} onClick={()=>chooseOption(correctAnswer)} ></FontAwesomeIcon><ToastContainer></ToastContainer></p>
+            <p><FontAwesomeIcon className ='text-sky-800' icon={faEye} onClick={()=>chooseOption(correctAnswer)} ></FontAwesomeIcon><ToastContainer></ToastContainer></p>
         </div> 
-        <div className='grid sm:grid-cols-1 lg:grid-cols-2'>{options.map(option=><Option option={option} correctAnswer={correctAnswer}></Option>)}
+        <div className='grid sm:grid-cols-1 lg:grid-cols-2 gap-4'>{options.map(option=><Option option={option} correctAnswer={correctAnswer}></Option>)}
         </div>   
         </div>
     );
